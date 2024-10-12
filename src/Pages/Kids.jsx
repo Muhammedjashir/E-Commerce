aba2454d65b333bafaf10798848aca52afa9360d
Muhammedjashir <jashirmhd71@gmail.com>
2024-10-12 15:52:15 +0530

    Kids

--- src/Pages/Kids.jsx
+++ src/Pages/Kids.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react' 
 import Navbar from '../MainComponent/Navbar'
 import { Link } from 'react-router-dom'
 function Kids() {
