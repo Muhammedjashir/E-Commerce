b090a5f0a3b2684e115af25840e11a7064fdf352
Muhammedjashir <jashirmhd71@gmail.com>
2024-10-14 10:07:34 +0530

    Warning 404 Page

--- src/PageNotfound.jsx
+++ src/PageNotfound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function PageNotfound() {
   return (
-    <div className='text-red-600 text-center mt-20 text-4xl '>
+    <div className='text-red-600 text-center mt-20 text-4xl'>
       <h1>Wrong data Page is not Fount!</h1>
     </div>
   )
