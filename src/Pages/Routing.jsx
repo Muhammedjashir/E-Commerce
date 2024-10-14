4f6d321df3dbc62867bf6d3a64675ebba4cbcc11
Muhammedjashir <jashirmhd71@gmail.com>
2024-10-14 10:05:16 +0530

    Routing

--- src/Pages/Routing.jsx
+++ src/Pages/Routing.jsx
@@ -4,6 +4,7 @@ import { Navigate } from 'react-router-dom';
 function Routing({ children }) {
   const token = localStorage.getItem('token');
   return token ? children : <navigate to="/signin" />;
+  
 }
 
 export default Routing;
